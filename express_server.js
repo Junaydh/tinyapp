f7270e48954ebc326fed09d60930eb009e802e02
Junayd H <junaydhus@gmail.com>
2023-01-25 16:04:00 -0500

    included basic logic to handle post request from urls/new and implemented a function to generate the shortURL id string

--- express_server.js
+++ express_server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,12 +17,21 @@ const urlDatabase = {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 })
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
-})
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
