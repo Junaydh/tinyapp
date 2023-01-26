b3250a14878088bfb280c4c7756105932a94c3a9
Junayd H <junaydhus@gmail.com>
2023-01-26 16:40:10 -0500

    added an input to the header partial that stores the given username in a cookie and displays it

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -8,6 +9,7 @@ const generateRandomString = function() {
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,7 +17,7 @@ const urlDatabase = {
 };
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render('urls_index', templateVars);
 });
 
@@ -31,11 +33,12 @@ app.post('/urls/:id/delete', (req, res) => {
 })
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = {username: req.cookies['username']};
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies['username']};
   res.render("urls_show", templateVars);
 });
 
@@ -60,6 +63,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post('/login' ,(req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls')
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
